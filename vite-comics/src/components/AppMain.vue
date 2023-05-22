43d84eb491ca2cda6d037c46242d937b6a293d1f
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-22 07:02:00 +0200

    Add Main

--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -17,3 +17,21 @@ export default {
     }
 }
 </script>
+
+<template>
+    <div class="main-body">
+        <p> ---> Contents goes here! <--- </p>
+    </div>
+</template>
+
+
+<style scoped>
+.main-body {
+    background-color: black;
+    color: white;
+}
+
+p {
+    font-size: 1.5 rem;
+}
+</style>
