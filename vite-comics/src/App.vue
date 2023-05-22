dd61ae05641b798e244e0187457518775da7eaf6
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-22 07:01:06 +0200

    fix

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -1,12 +1,15 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppBlueBand from './components/AppBlueBand.vue';
 import AppFooter from './components/AppFooter.vue';
+
 // import HelloWorld from './components/HelloWorld.vue'
 export default {
   components: {
     AppHeader,
     AppMain,
+    AppBlueBand,
     AppFooter
   },
   data() {
@@ -27,20 +30,28 @@ export default {
 <template>
   <AppHeader />
   <AppMain />
+  <AppBlueBand />
   <AppFooter />
 </template>
 
 <style scoped>
-/* .logo {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-} */
-</style>
+}</style>
