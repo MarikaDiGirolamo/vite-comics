3dea0d80df107d977359261097197526a48ff616
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-22 07:01:23 +0200

    Add AppBlueBand

---
+++ vite-comics/src/components/AppBlueBand.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: AppBlueBand,
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods() {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div class="blue-band">
+        <div class="merchindise">
+            <a href="">Digital comics</a>
+            <a href="">DC Merchandise</a>
+            <a href="">Subscription</a>
+            <a href="">Comic Shop Locator</a>
+            <a href="">DC POWER VISA</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.blue-band {
+    background-color: #0282f9;
+    color: white;
+}
+</style>
